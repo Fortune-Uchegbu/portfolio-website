54a900146ae6d510d4e28ab3ff01253225c51cd7
Pandapacific <fortuneski88@gmail.com>
2025-11-29 10:28:03 +0100

    fix: adjust ProjectCard component styling for consistency and improved layout

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 
 const ProjectCard = ({ project }) => {
   return (
-    <article className=' border border-neutral-dark dark:border-neutral-light rounded-xl shadow-md hover:shadow-2xl dark:hover:shadow-lg transition-all duration-300 dark:hover:shadow-accent-dark hover:scale-[1.005] ease-in-out'>
+    <article className=' border border-neutral-dark dark:border-neutral-light rounded-xl overflow-hidden shadow-md hover:shadow-2xl dark:hover:shadow-lg transition-all duration-300 dark:hover:shadow-accent-dark hover:scale-[1.005] ease-in-out'>
       <img 
-      className='w-full rounded-t-xl'
+      className='w-full'
       src={project.image} alt={`${project.title} image`} />
       <section className="p-6 flex flex-col justify-between gap-y-3">
-        <h3 className="font-medium my-4 text-2xl ">{project.title}</h3>
+        <h3 className="font-medium my-2 text-2xl ">{project.title}</h3>
         <a>{project.description}</a>
         <p>
           <span className='font-semibold'>Stack: </span>
@@ -17,7 +17,7 @@ const ProjectCard = ({ project }) => {
           {Object.entries(project.links).map(([key, value]) => (
             <li key={key} className='min-w-full'>
               <span className='font-semibold'> {`${key}: `}</span>
-              <a className='hover:underline underline-offset-2' href={value} target="_blank" rel="noopener noreferrer">
+              <a className='hover:underline active:underline underline-offset-2' href={value} target="_blank" rel="noopener noreferrer">
                 {value}
               </a>
             </li>
